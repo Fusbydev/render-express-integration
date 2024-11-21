4f30125ff07a27353dc54a8a1a2152ab46a7ac22
Fusbydev <kirkfabonnnn@gmail.com>
2024-11-21 23:39:34 +0800

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const productService = require('./product-services');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 // Middleware
 app.use(express.json());
 
